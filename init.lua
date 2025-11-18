f77f7d8a0390e5a09de0d7ed9043d9b06294b974
mantasgh <129780580+mantasgh@users.noreply.github.com>
2025-11-18 23:42:40 +0200

    configure gutter; enable gui colors

--- init.lua
+++ init.lua
@@ -1,6 +1,14 @@
 local bin_path = vim.fn.expand("~/.config/nvim/bin/node_modules/.bin")
 vim.env.PATH = bin_path .. ":" .. vim.env.PATH
 
+-- The 'termguicolors' option is false in init.lua; it is updated asynchronously after truecolor detection.
+-- Enable it here so colorschemes use truecolor from the start.
+vim.opt.termguicolors = true
+
+-- gutter
+vim.opt.number = true
+vim.opt.signcolumn = 'yes'
+
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect', 'noinsert', 'popup' }
 
 vim.lsp.config.typescript = {
