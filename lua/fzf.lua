6fb3c9262b05b74b6452760e307bd9b9b0498795
mantasgh <129780580+mantasgh@users.noreply.github.com>
2026-03-22 20:30:08 +0200

    refactor fzf

--- lua/fzf.lua
+++ lua/fzf.lua
@@ -15,7 +15,39 @@ local function create_split()
     return nil, 'failed to create split window'
   end
 
-  return { buffer_id = buffer_id, window_id = window_id }, nil
+  local function cleanup() 
+    if vim.api.nvim_win_is_valid(window_id) then
+      vim.api.nvim_win_close(window_id, true)
+    end
+
+    if vim.api.nvim_buf_is_valid(buffer_id) then
+      vim.api.nvim_buf_delete(buffer_id, { force = true })
+    end
+  end
+
+  return { buffer_id = buffer_id, window_id = window_id, cleanup = cleanup }, nil
+end
+
+local function process_stdout(buffer_id, exit_code)
+  local SIGINT = 130
+  if exit_code == SIGINT then
+    return nil, nil
+  end
+
+  if exit_code ~= 0 then
+    return nil, string.format('process exited with code %d', exit_code)
+  end
+
+  local line = 0
+  local strict = false
+  local ok, lines = pcall(vim.api.nvim_buf_get_lines, buffer_id, line, line + 1, strict)
+
+  if not ok or #lines == 0 then
+    return nil, 'failed to read lines'
+  end
+
+  local result = lines[1]
+  return result, nil
 end
 
 local function fzf(callback)
@@ -25,50 +57,17 @@ local function fzf(callback)
     return
   end
 
-  local function cleanup() 
-    if vim.api.nvim_win_is_valid(split.window_id) then
-      vim.api.nvim_win_close(split.window_id, true)
-    end
-
-    if vim.api.nvim_buf_is_valid(split.buffer_id) then
-      vim.api.nvim_buf_delete(split.buffer_id, { force = true })
-    end
-  end
-
   local job_id = vim.fn.jobstart({ 'fzf' }, {
     term = true,
     on_exit = function(_job_id, exit_code, _event_type) 
-      local SIGINT = 130
-      if exit_code == SIGINT then
-	      cleanup()
-	      callback(nil, nil)
-        return
-      end
-
-      if exit_code ~= 0 then
-        cleanup()
-        callback(nil, string.format('process exited with code %d', exit_code))
-        return
-      end
-
-      local line = 0
-      local strict = false
-      local ok, lines = pcall(vim.api.nvim_buf_get_lines, split.buffer_id, line, line + 1, strict)
-      
-      if not ok or #lines == 0 then
-        cleanup()
-        callback(nil, 'failed to read lines')
-        return
-      end
-
-      local result = lines[1]
-      cleanup()
-      callback(result, nil)
+      local result, error = process_stdout(split.buffer_id, exit_code)
+      split.cleanup()
+      callback(result, error)
     end 
   })
 
   if job_id <= 0 then
-    cleanup()
+    split.cleanup()
     callback(nil, 'failed to start job')
     return
   end
